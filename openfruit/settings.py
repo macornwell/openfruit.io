0cf430d4118a532723dbfc2292d438f0c173e601
Mike Cornwell <mike@bonnieblueag.com>
2017-04-25 10:08:47 -0500

    Fixed settings.

--- openfruit/settings.py
+++ openfruit/settings.py
@@ -10,8 +10,8 @@ For the full list of settings and their values, see
 https://docs.djangoproject.com/en/1.8/ref/settings/
 """
 
-# Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 import os
+from django.contrib import messages
 
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 
