3f1f17bbd6d9a3c95ef5cdd6cabe413bab1c34ee
Mike Cornwell <mike@bonnieblueag.com>
2017-04-25 10:29:43 -0500

    Added static reference.

--- openfruit/urls.py
+++ openfruit/urls.py
@@ -12,6 +12,7 @@ Class-based views
 Including another URLconf
     1. Add a URL to urlpatterns:  url(r'^blog/', include('blog.urls'))
 """
+from django.conf.urls.static import static
 from django.conf.urls import include, url
 from django.contrib import admin
 
