1558454c65d11344dcfda1b8441de81e2a1046ee
Mike Cornwell <mike@bonnieblueag.com>
2017-08-26 15:16:22 -0500

    Started working on readme.

--- openfruit/dashboard/views.py
+++ openfruit/dashboard/views.py
@@ -6,9 +6,6 @@ from openfruit.reports.useractivity.services import USER_ACTIVITY_DAL
 
 def dashboard(request):
     data = {}
-    users_plants = USER_ACTIVITY_DAL.get_all_living_plants_interacted_by(request.user)
     species_with_gmaps_icon = TAXONOMY_DAL.get_species_with_google_maps_images()
-    data['users_plants'] = users_plants
-    data['species_list'] = set(plant.species for plant in users_plants)
     data['species_to_icon'] = species_with_gmaps_icon
     return render(template_name='dashboard.html', context=data, request=request)
