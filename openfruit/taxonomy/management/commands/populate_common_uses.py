196019968ea99da05df7cca097b8eb6c04379b91
Michael Cornwell <mike@bonnieblueag.com>
2018-02-04 11:33:51 -0600

    New fixes.

---
+++ openfruit/taxonomy/management/commands/populate_common_uses.py
@@ -0,0 +1,22 @@
+from django.core.management.base import BaseCommand
+from openfruit.taxonomy.models import FruitUsageType
+
+COMMON_USES = [
+    'Baking',
+    'Cooking',
+    'Cider',
+    'Drying',
+    'Fresh Eating',
+    'Juice',
+    'Preserves',
+]
+
+
+class Command(BaseCommand):
+    help = "Creates all of the default Fruit Uses."
+
+    def handle(self, *args, **options):
+        for use in COMMON_USES:
+            obj, made = FruitUsageType.objects.get_or_create(type=use)
+            if made:
+                print('Created {0}'.format(made))
