67d7e51391c20f714b2ca10e32f8452ca9221a97
Michael Cornwell <michael@opencraft.com>
2018-02-19 12:06:43 -0600

    GUI updates.

--- openfruit/fruit_search/views.py
+++ openfruit/fruit_search/views.py
@@ -7,7 +7,7 @@ jwt_encode_handler = api_settings.JWT_ENCODE_HANDLER
 
 from openfruit.taxonomy.serializers import CultivarSerializer
 from openfruit.fruit_search.services import FRUIT_SEARCH_SERVICE
-from openfruit.taxonomy.models import COMMON_USES, RIPENING_MONTH_CHOICES
+from openfruit.taxonomy.models import COMMON_USES, RIPENING_MONTH_CHOICES, CHROMOSOME_CHOICES
 from django_geo_db.services import GEO_DAL
 from openfruit.fruit_reference.services import FRUIT_REFERENCE_SERVICE
 
@@ -22,6 +22,7 @@ def fruit_search(request):
         'RIPENINGS': RIPENING_MONTH_CHOICES,
         'STATES': GEO_DAL.get_us_states(),
         'TOKEN': token,
+        'CHROMOSOMES': [c[0] for c in CHROMOSOME_CHOICES]
     }
     return render(request, template_name='fruit_search/search-and-filter.html', context=data)
 
@@ -42,6 +43,7 @@ class FruitSearchListView(ListAPIView):
         ripening_low = params.get('ripening_low', None)
         ripening_high = params.get('ripening_high', None)
         books = params.get('books', None)
+        chromosomes = params.get('chromosomes', None)
         reference_id = []
         if books:
             for book in books:
@@ -49,7 +51,7 @@ class FruitSearchListView(ListAPIView):
         results = FRUIT_SEARCH_SERVICE.filter(
             species=species, state=state, use_list=use_list,
             year_low=year_low, year_high=year_high, ripening_low=ripening_low,
-            ripening_high=ripening_high, reference_id=reference_id
+            ripening_high=ripening_high, reference_id=reference_id, chromosomes=chromosomes
         )
         return results
 
