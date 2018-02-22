4d1a18d774b003d17c34cb9ea27baad7124a5f0d
Michael Cornwell <mike@bonnieblueag.com>
2018-02-22 12:19:30 -0600

    Added fix script. Added author to admin.

--- openfruit/fruit_reference/admin.py
+++ openfruit/fruit_reference/admin.py
@@ -1,5 +1,6 @@
 from django.contrib import admin
-from openfruit.fruit_reference.models import FruitReference, FruitReferenceType
+from openfruit.fruit_reference.models import FruitReference, FruitReferenceType, Author
 
 admin.site.register(FruitReference)
 admin.site.register(FruitReferenceType)
+admin.site.register(Author)
