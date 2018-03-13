fe3c1c837e2a30c63d061792b7e6c69db3b337cf
Michael Cornwell <michael.a.cornwell@gmail.com>
2018-03-13 14:20:35 -0500

    Fix.

--- openfruit/geography/management/commands/populate_maps_for_cultivars.py
+++ openfruit/geography/management/commands/populate_maps_for_cultivars.py
@@ -10,7 +10,6 @@ class Command(BaseCommand):
     help = "Pre-Populates all maps for every cultivar, if possible. MUST HAVE Site set correctly."
 
     def handle(self, *args, **options):
-        domain = Site.objects.all().first().domain
         map_type = GEO_DAL.get_map_type('simple')
         for c in Cultivar.objects.all():
             location = c.origin_location
