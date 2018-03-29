13f49a7e4729f1d4fd88a43629a4a388a63d94c0
Michael Cornwell <michael.a.cornwell@gmail.com>
2018-03-28 19:07:51 -0500

    Changes for redis.

--- Procfile
+++ Procfile
@@ -1,2 +1,2 @@
 web: gunicorn openfruit.wsgi --log-file -
-worker: celery worker --app=django_geo_db.tasks
+worker: celery worker --app=django_geo_db.celery
--- openfruit/settings.py
+++ openfruit/settings.py
@@ -296,7 +296,11 @@ OF_API_PASSWORD = os.environ.get('OF_API_PASSWORD', '')
 ############
 CORS_ORIGIN_ALLOW_ALL = True
 CORS_ALLOW_CREDENTIALS = True
-
+CELERY_BROKER_URL = 'redis://localhost:6379'
+CELERY_RESULT_BACKEND = 'redis://localhost:6379'
+CELERY_ACCEPT_CONTENT = ['application/json']
+CELERY_RESULT_SERIALIZER = 'json'
+CELERY_TASK_SERIALIZER = 'json'
 
 
 CLOUDFRONT_DOMAIN = os.environ.get('CLOUDFRONT_DOMAIN', '')
